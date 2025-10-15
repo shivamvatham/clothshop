b69db3c0927d703ca283ce523259a23f0868b944
shivamvatham <shivam.r.vatham@gmail.com>
2025-10-15 11:02:10 +0530

    nuxt ui setup added

--- frontend/app/app.config.ts
+++ frontend/app/app.config.ts
@@ -1,10 +1,10 @@
 export default defineAppConfig({
-    title: 'cloth shop',
-    theme: {
-      dark: true,
-      colors: {
-        primary: '#ff0000'
-      }
-    }
-  })
+  title: 'cloth shop',
+  ui: {
+    primary: 'red',
+    gray: 'neutral',
+    colors: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose'],
+    strategy: 'merge'
+  }
+})
   
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'], // Path to your main CSS file
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  colorMode: {
+    preference: 'light'
+  },
+  ui: {
+    global: true
   },
   runtimeConfig: {
     public: {
